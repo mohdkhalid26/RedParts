9f66ea26edf63278fdfcb1ead701d9f8f63ba9cb
“mohdkhalid26” <mohdkhalid211003@gmail.com>
2024-02-03 01:45:19 +0530

    sixth commit

--- src/modules/public/component/slider/Slider.jsx
+++ src/modules/public/component/slider/Slider.jsx
@@ -3,8 +3,8 @@ import { useEffect, useRef, useState } from "react";
 function Slider() {
   const imagesRef = useRef();
   let conRef = useRef(true);
-  const [conRight, SetConRight] = useState(false);
-  const [conLeft, SetConLeft] = useState(true);
+  const [conRight, SetConRight] = useState(true);
+  const [conLeft, SetConLeft] = useState(false);
   const [timeOut, SetTimeOut] = useState(null);
   const [funcOut, SetFuncOut] = useState(null);
   const [scrollVal, SetScrollVal] = useState(0);
@@ -32,42 +32,63 @@ function Slider() {
     },
     {
       img: "https://tse2.mm.bing.net/th?id=OIP.BK6NQWcIA5Ko8FLiDo12tgHaHa&pid=Api&P=0&h=180",
+    },{
+      img: "https://tse2.mm.bing.net/th?id=OIP.rQfjNU7I1vu4rkTIVIjo4QHaHa&pid=Api&P=0&h=180",
+    },
+    {
+      img: "https://tse3.mm.bing.net/th?id=OIP.BU8oZa8tjDtujhZ6Lg4ptgHaHa&pid=Api&P=0&h=180",
+    },
+    {
+      img: "https://tse2.mm.bing.net/th?id=OIP.SP5-nQIZlOxP_h9Des6DDAHaHa&pid=Api&P=0&h=180",
+    },
+    {
+      img: "https://tse2.mm.bing.net/th?id=OIP.BK6NQWcIA5Ko8FLiDo12tgHaHa&pid=Api&P=0&h=180",
+    },
+    {
+      img: "https://tse2.mm.bing.net/th?id=OIP.rQfjNU7I1vu4rkTIVIjo4QHaHa&pid=Api&P=0&h=180",
+    },
+    {
+      img: "https://tse3.mm.bing.net/th?id=OIP.BU8oZa8tjDtujhZ6Lg4ptgHaHa&pid=Api&P=0&h=180",
+    },
+    {
+      img: "https://tse2.mm.bing.net/th?id=OIP.SP5-nQIZlOxP_h9Des6DDAHaHa&pid=Api&P=0&h=180",
+    },
+    {
+      img: "https://tse2.mm.bing.net/th?id=OIP.BK6NQWcIA5Ko8FLiDo12tgHaHa&pid=Api&P=0&h=180",
     },
   ]);
 
   function getScrollMaxVal() {
-    imagesRef.current.scrollLeft += 999999999;
-    setTimeout(() => {
-      let val = imagesRef.current.scrollLeft;
-      SetScrollVal(val);
-    }, 1000);
-  }
+    let wid = imagesRef.current.clientWidth - imagesRef.current.scrollWidth;
+    let posWid = wid * -1;
+    SetScrollVal(posWid);
+    }
   useEffect(() => {
     getScrollMaxVal();
   }, []);
   function funcLeft() {
-    let t = 1600;
+    let t = 60;
     let time = setInterval(() => {
       if (imagesRef.current.scrollLeft <= 1) {
         conRef.current = true;
         SetConLeft(false);
         SetConRight(true);
       } else {
-        imagesRef.current.scrollLeft -= 160;
+        imagesRef.current.scrollLeft -= 0.50;
       }
     }, t);
     SetTimeOut(time);
     SetFuncOut({ func: funcLeft });
   }
   function funcRight() {
-    let t = 1600;
+    let t = 60;
     let time = setInterval(() => {
       if (imagesRef.current.scrollLeft >= scrollVal - 1) {
         conRef.current = true;
         SetConLeft(true);
         SetConRight(false);
       } else {
-        imagesRef.current.scrollLeft += 160;
+        imagesRef.current.scrollLeft += 1;
       }
     }, t);
     SetTimeOut(time);
@@ -80,12 +101,12 @@ function Slider() {
         setTimeout(() => {
           clearInterval(timeOut);
           funcRight();
-        }, 1500);
+        }, 1000);
       } else {
         setTimeout(() => {
           clearInterval(timeOut);
           funcLeft();
-        }, 1500);
+        }, 1000);
       }
     }
   }, [conLeft, conRight]);
@@ -93,7 +114,7 @@ function Slider() {
     <div
       className="slider"
       onMouseEnter={() => clearInterval(timeOut)}
-      onMouseLeave={() => funcOut.func()}
+      onMouseLeave={() => funcOut !== null ? funcOut.func():""}
     >
       <div ref={imagesRef} className="images">
         {images?.map((i, index) => {
