cd18d886e6c23a87652bc82802f33eebed7961bc
“mohdkhalid26” <mohdkhalid211003@gmail.com>
2024-02-07 14:51:23 +0530

    tenth commit

--- src/modules/public/component/footer/Footer.jsx
+++ src/modules/public/component/footer/Footer.jsx
@@ -3,7 +3,15 @@ import "./Footer.scss"
 
 function Footer() {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+        <div className="first">
+
+        <div className="about">a</div>
+        <div className="contact">c</div>
+        </div>
+        <div className="sec">m</div>
+        <div className="third">t</div>
+    </div>
   )
 }
 
