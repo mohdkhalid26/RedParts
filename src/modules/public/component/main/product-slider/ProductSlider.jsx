8dfa80129a9e4c48b4148914994fb0486202e97b
“mohdkhalid26” <mohdkhalid211003@gmail.com>
2024-02-09 18:30:47 +0530

    15th commit

--- src/modules/public/component/main/product-slider/ProductSlider.jsx
+++ src/modules/public/component/main/product-slider/ProductSlider.jsx
@@ -126,6 +126,10 @@ function ProductSlider() {
 
   return (
       <div
+      onTouchMoveCapture={() => timeOut !== null ? clearInterval(timeOut):""}
+      onTouchMove={() => timeOut !== null ? clearInterval(timeOut):""}
+      onTouchStartCapture={() => timeOut !== null ? clearInterval(timeOut):""}
+      onTouchStart={() => timeOut !== null ? clearInterval(timeOut):""}
       onMouseOver={() => timeOut !== null ? clearInterval(timeOut):""}
       onMouseEnter={() => timeOut !== null ? clearInterval(timeOut):""}
       onMouseLeave={() => funcOut !== null ? funcOut.func():""}
